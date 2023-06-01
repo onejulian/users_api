83053d5dd5f7bd18b363f01ec6269c10f82609a0
julianpulzo <julian.correa@pulzo.com>
2023-06-01 01:29:31 -0500

    :wrench: Actualiza configuración de cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,6 +15,10 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
+        header('Access-Control-Allow-Origin:  *');
+        // header('Access-Control-Allow-Origin:  http://localhost:4200');
+        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+        header('Access-Control-Allow-Methods:  POST, PUT');
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*');
 

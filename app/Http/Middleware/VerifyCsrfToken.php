bcff7ff2ab49423fe871eea2504063c6f291f4fd
julianpulzo <julian.correa@pulzo.com>
2023-06-01 00:47:18 -0500

    :wrench: Agrega dominio del front para permitir las peticiones

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'https://glowing-cranachan-0a3886.netlify.app/*',
     ];
 }
